638119566eaf272a719eee0185f710aed8051271
meghana-meher <102056615+meghana-meher@users.noreply.github.com>
2023-10-26 10:19:58 +0000

    variables and datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail="megha@gmail.com"
+var accountPassword="12345"
+accountCity="Hyderabad"
+let accountState;
+
+
+// accountId=3 //not allowed
+accountEmail="meg@gg.com"
+accountPassword="11111"
+accountCity="delhi"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser 
+// console.log(3+3) console.log("meghana");//error
+// console.log(3+3) ;console.log("meghana")// bad practice
+console.log(3+3) 
+console.log("meghana")//good practice
+
+
+
